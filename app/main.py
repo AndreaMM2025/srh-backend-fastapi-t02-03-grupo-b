312cfb887aae36b97cf19231322dd51a416458c6
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-20 04:30:18 -0500

    Andrea: registrar router habitaciones

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import clientes
+from app.routers import clientes, habitaciones
 
 app = FastAPI(title="SRH Backend")
 
@@ -8,4 +8,6 @@ def root():
     return {"message": "API funcionando correctamente"}
 
 app.include_router(clientes.router)
+app.include_router(habitaciones.router)
+
 # para visualizar la pagina del srh backend http://127.0.0.1:8000/docsv
