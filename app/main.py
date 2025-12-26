d5f7d4d3e7d72ea43735e0457575bf2ea683b84d
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-25 20:25:35 -0500

    Actualización en el main.py para arranque del backend

--- app/main.py
+++ app/main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
-from app.routers import clientes, habitaciones, reservas, facturas, pagos, reportes
+from app.routers import (
+    clientes,
+    habitaciones,
+    reservas,
+    facturas,
+    pagos,
+    reportes,
+    usuarios   # 👈 NUEVO
+)
 
-app = FastAPI(title="Sistema de Reservas de Hoteles (SRH) - Backend")
+app = FastAPI(
+    title="Sistema de Reservas de Hoteles (SRH) - Backend"
+)
 
 @app.get("/")
 def root():
     return {"message": "API funcionando correctamente"}
 
+# Routers
 app.include_router(clientes.router)
 app.include_router(habitaciones.router)
 app.include_router(reservas.router)
 app.include_router(facturas.router)
 app.include_router(pagos.router)
 app.include_router(reportes.router)
+app.include_router(usuarios.router) 
 
 """
 Documentación del backend SRH:
