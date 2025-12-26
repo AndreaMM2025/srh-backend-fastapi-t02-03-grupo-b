efe7d6f56e6d072d588199e8ffeee7374d9dc83b
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-25 20:13:47 -0500

    Actualización del modelo factura

--- app/models/factura.py
+++ app/models/factura.py
@@ -1,10 +1,10 @@
+# Modelo: Factura
 from typing import Optional
 from pydantic import BaseModel
-from datetime import date
 
 class Factura(BaseModel):
     id: Optional[int] = None
-    cliente_id: int
     reserva_id: int
+    subtotal: float
+    impuestos: float
     total: float
-    fecha: date
