f8389f7aab86bf86e1fdebecef2c57126e49c068
Andrea Murillo <andreamurillomedina110@gmail.com>
2026-01-08 23:22:45 -0500

    Agregar pruebas de reportes

--- app/test/test_reportes.py
+++ app/test/test_reportes.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_listar_reportes():
-    response = client.get("/api/reportes/")
+    response = client.get("/api/reportes/ingresos")
     assert response.status_code == 200, response.text
     assert isinstance(response.json(), list)
